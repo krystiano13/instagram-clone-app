dc8d8741e84a71c2ec0e65917c35943709958942
krystiano13 <crystianotv@gmail.com>
2024-10-23 18:28:37 +0200

    fix app scroll

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -12,20 +12,20 @@ export function Layout({ children }: Props) {
   const [navbarOpen, setNavbarOpen] = useState(false)
 
   return (
-    <div className="w-[100vw] h-[100vh] bg-slate-50 overflow-x-hidden">
-      <main className="w-[100vw] h-[100vh] flex flex-col sm:flex-row">
+    <div className="w-full h-[100vh] bg-slate-50">
+      <main className="w-full h-[100vh] flex flex-col sm:flex-row">
         {location.pathname !== "/login" &&
           location.pathname !== "/register" && (
             <>
               <div
                 id="menu"
-                className={`w-[100vw] h-[100vh] sm:hidden ${
+                className={`w-full h-[100vh] sm:hidden ${
                   !navbarOpen ? "translate-y-[-100%]" : "translate-y-[5%]"
                 } fixed z-50 sm:translate-y-[0%] transition`}
               >
                 <DesktopButtons />
               </div>
-              <div className="w-[100vw] h-[100vh] hidden sm:block fixed transition">
+              <div className="w-full h-[100vh] hidden sm:block fixed transition">
                 <DesktopButtons />
               </div>
               <div className="card fixed z-50 flex flex-row p-4 justify-between items-center sm:hidden w-[100vw] h-[3rem]">
--- frontend/src/views/home.tsx
+++ frontend/src/views/home.tsx
@@ -2,7 +2,7 @@ import { PostPreview } from "../components/shared/post-preview"
 
 export function Home() {
   return (
-    <div className="w-calc h-[100vh] flex flex-col items-center p-6 gap-12">
+    <div className="w-calc min-h-[100vh] flex flex-col items-center p-6 gap-12 bg-slate-50">
       <PostPreview />
       <PostPreview />
     </div>
