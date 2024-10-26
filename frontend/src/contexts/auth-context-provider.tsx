c30b7bd5213bc0ecd89602c7a5ee4097566d4221
krystiano13 <crystianotv@gmail.com>
2024-10-26 12:01:36 +0200

    fix redirect when no token provided

--- frontend/src/contexts/auth-context-provider.tsx
+++ frontend/src/contexts/auth-context-provider.tsx
@@ -1,66 +1,68 @@
-import {createContext, useEffect, useState} from "react";
-import {useLocation, useNavigate} from "react-router";
+import { createContext, useEffect, useState } from "react"
+import { useLocation, useNavigate } from "react-router"
 
 const initialValue = {
-    isLogged: false,
-    setIsLogged: (value: boolean) => {
-        console.log(value)
-    }
+  isLogged: false,
+  setIsLogged: (value: boolean) => {
+    console.log(value)
+  },
 }
 
 export const AuthContext = createContext(initialValue)
 
 interface Props {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
-export function AuthContextProvider({children}: Props) {
-    const [isLogged, setIsLogged] = useState<boolean>(false)
-    const [firstRender, setFirstRender] = useState<boolean>(false)
-    const location = useLocation()
-    const navigate = useNavigate()
+export function AuthContextProvider({ children }: Props) {
+  const [isLogged, setIsLogged] = useState<boolean>(false)
+  const [firstRender, setFirstRender] = useState<boolean>(false)
+  const location = useLocation()
+  const navigate = useNavigate()
 
-    useEffect(() => {
-        if(!firstRender) return
-        if (location.pathname === "/login" || location.pathname === "/register") {
-            if (isLogged) {
-                navigate("/")
-            }
-        } else {
-            if (!isLogged) {
-                navigate("/login")
-            }
-        }
-    }, [location.pathname])
+  useEffect(() => {
+    if (!firstRender) return
+    if (location.pathname === "/login" || location.pathname === "/register") {
+      if (isLogged) {
+        navigate("/")
+      }
+    } else {
+      if (!isLogged) {
+        navigate("/login")
+      }
+    }
+  }, [location.pathname])
 
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        const id = localStorage.getItem("id")
-        const username = localStorage.getItem("username")
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    const id = localStorage.getItem("id")
+    const username = localStorage.getItem("username")
 
-        if (!token || !id || !username) return;
+    if (!token || !id || !username) {
+      navigate("/login")
+      return
+    }
 
-        fetch("http://127.0.0.1:3000/user", {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json"
-            }
-        })
-            .then(res => {
-                if (res.ok) {
-                    setIsLogged(true)
-                } else {
-                    setIsLogged(false)
-                }
-                setFirstRender(true)
-            })
+    fetch("http://127.0.0.1:3000/user", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        setIsLogged(true)
+      } else {
+        setIsLogged(false)
+        navigate("/login")
+      }
+      setFirstRender(true)
+    })
+  }, [])
 
-    }, [])
-
-    return (
-        <AuthContext.Provider value={{isLogged, setIsLogged}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={{ isLogged, setIsLogged }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
