7b03255248e377a4e820c332e209e7bbf2edf538
krystiano13 <crystianotv@gmail.com>
2024-10-30 17:38:51 +0100

    fix y-scroll always visible

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -12,20 +12,20 @@ export function Layout({ children }: Props) {
   const [navbarOpen, setNavbarOpen] = useState(false)
 
   return (
-    <div className="w-[100vw] h-[100vh] bg-slate-50 overflow-x-hidden">
-      <main className="w-[100vw] h-[100vh] flex flex-col sm:flex-row">
+    <div className="w-[100vw] bg-slate-50 overflow-x-hidden">
+      <main className="w-[100vw] flex flex-col sm:flex-row">
         {location.pathname !== "/login" &&
           location.pathname !== "/register" && (
             <>
               <div
                 id="menu"
-                className={`w-auto h-[100vh] sm:hidden ${
+                className={`w-auto sm:hidden ${
                   !navbarOpen ? "translate-y-[-100%]" : "translate-y-[5%]"
                 } fixed z-50 sm:translate-y-[0%] transition`}
               >
                 <DesktopButtons />
               </div>
-              <div className="w-auto h-[100vh] hidden sm:block fixed transition">
+              <div className="w-auto hidden sm:block fixed transition">
                 <DesktopButtons />
               </div>
               <div className="card fixed z-50 flex flex-row p-4 justify-between items-center sm:hidden w-[100vw] h-[3rem]">
@@ -48,7 +48,7 @@ export function Layout({ children }: Props) {
             "sm:ml-[20rem]"
           }`}
         >
-          <section className="z-100 content mt-12">{children}</section>
+          <section className="z-100 content">{children}</section>
         </div>
       </main>
     </div>
