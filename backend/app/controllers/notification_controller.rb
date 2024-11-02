a56faa9be322c5cf37a3aac271cc947b6423d3bb
krystiano13 <crystianotv@gmail.com>
2024-11-02 18:37:40 +0100

    implement creating notification messages

--- backend/app/controllers/notification_controller.rb
+++ backend/app/controllers/notification_controller.rb
@@ -1,8 +1,21 @@
 class NotificationController < ApplicationController
   def index
     notifications = Notification.where(user_id: params[:user_id])
+    notifications_with_username = []
+
+    notifications.each do |notification|
+      user = User.find_by(id: notification[:sender_id])
+      notifications_with_username.unshift(
+        {
+          user_id: notification[:user_id],
+          sender_id: notification[:sender_id],
+          content: "#{user[:user_name]} #{notification[:content]}"
+        }
+      )
+    end
+
     render json: {
-      notifications: notifications
+      notifications: notifications_with_username
     }, status: :ok
   end
 
