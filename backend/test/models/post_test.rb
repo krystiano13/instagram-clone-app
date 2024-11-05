f37ca4801c9f4ce7e8e4e4dfe1ffffd804728b7f
krystiano13 <crystianotv@gmail.com>
2024-11-05 21:20:30 +0100

    write first tests for post model

--- backend/test/models/post_test.rb
+++ backend/test/models/post_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should show error when all columns are not provided" do
+    post = Post.new
+    assert_not post.save
+  end
+
+  test "should create post" do
+    image = file_fixture("test.png")
+
+    post = Post.new(user_id: 1, description: "desc", image: image)
+    assert post.save
+  end
 end
