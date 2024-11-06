d864fce35da4e852d7113d3e3c9562cb2e3ab975
krystiano13 <crystianotv@gmail.com>
2024-11-06 12:45:09 +0100

    write tests for follower model

--- backend/test/models/follower_test.rb
+++ backend/test/models/follower_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class FollowerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create follower" do
+    follower = Follower.new(user_id: 1, follower_id: 2)
+    assert follower.save
+  end
+
+  test "should not create follower" do
+    follower_empty = Follower.new
+    follower_without_user = Follower.new(follower_id: 2)
+    follower_without_follower_id = Follower.new(user_id: 1)
+
+    assert_not follower_empty.save
+    assert_not follower_without_follower_id.save
+    assert_not follower_without_user.save
+  end
 end
