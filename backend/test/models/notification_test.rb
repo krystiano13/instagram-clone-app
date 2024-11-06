5cd634c40b51478917409fd476077416b09d9624
krystiano13 <crystianotv@gmail.com>
2024-11-06 12:36:17 +0100

    write tests for post and notification model

--- backend/test/models/notification_test.rb
+++ backend/test/models/notification_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class NotificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create notification" do
+    notification = Notification.new(user_id: 1, sender_id: 2, content: "notification")
+    assert notification.save
+  end
+
+  test "should not create notification when user does not exists" do
+    notification = Notification.new(user_id: 0, sender_id: 2, content: "notification")
+    assert_not notification.save
+  end
+
+  test "should not create notification" do
+    notification = Notification.new(user_id: 1, sender_id: 2)
+    notification_without_sender = Notification.new(user_id: 1, content: "test")
+
+    assert_not notification.save
+    assert_not notification_without_sender.save
+  end
 end
--- backend/test/models/post_test.rb
+++ backend/test/models/post_test.rb
@@ -2,7 +2,21 @@ require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
   test "should show error when all columns are not provided" do
-    post = Post.new
+    image = file_fixture("test.png")
+
+    post_empty = Post.new
+    post = Post.new(user_id: 1, image: image)
+    post_without_image = Post.new(user_id: 1, description: "desc")
+
+    assert_not post_empty.save
+    assert_not post.save
+    assert_not post_without_image.save
+  end
+
+  test "should not create post when user does not exists" do
+    image = file_fixture("test.png")
+
+    post = Post.new(user_id: 0, description: "desc", image: image)
     assert_not post.save
   end
 
