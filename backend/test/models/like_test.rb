e7faf62f7505bb8fea5cca43f68ddbace8d4ed33
krystiano13 <crystianotv@gmail.com>
2024-11-06 12:40:10 +0100

    write tests for like model

--- backend/test/models/like_test.rb
+++ backend/test/models/like_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create like" do
+    like = Like.new(user_id: 1, post_id: 1)
+    assert like.save
+  end
+
+  test "should not create like" do
+    like_without_user = Like.new(post_id: 1)
+    like_without_post = Like.new(user_id: 1)
+    like_empty = Like.new
+
+    assert_not like_empty.save
+    assert_not like_without_post.save
+    assert_not like_without_user.save
+  end
 end
