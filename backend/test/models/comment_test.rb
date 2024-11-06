790041fff5f264a11e3f04ceba449940c6cc1c8b
krystiano13 <crystianotv@gmail.com>
2024-11-06 12:51:46 +0100

    write tests for comments

--- backend/test/models/comment_test.rb
+++ backend/test/models/comment_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create comment" do
+    comment = Comment.new(user_id: 1, post_id: 1, text: "Nice")
+    assert comment.save
+  end
+
+  test "should not create comment" do
+    comment_1 = Comment.new
+    comment_2 = Comment.new(user_id: 1, text: "text")
+    comment_3 = Comment.new(post_id: 1, text: "text")
+    comment_4 = Comment.new(user_id: 1, post_id: 1)
+
+    assert_not comment_1.save
+    assert_not comment_2.save
+    assert_not comment_3.save
+    assert_not comment_4.save
+  end
 end
