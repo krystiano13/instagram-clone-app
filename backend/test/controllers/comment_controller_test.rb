11157445b627aa63e37936006ebb3f365b79f07d
krystiano13 <crystianotv@gmail.com>
2024-11-07 18:48:33 +0100

    write tests for comments controller

--- backend/test/controllers/comment_controller_test.rb
+++ backend/test/controllers/comment_controller_test.rb
@@ -6,4 +6,28 @@ class CommentControllerTest < ActionDispatch::IntegrationTest
     get "/comments/1", headers: { Authorization: "Bearer #{create_mock_token}" }
     assert_response :success
   end
+
+  test "create action should return 200" do
+    create_mock_user
+    post "/comments", params: { text: "test", user_id: 1, post_id: 1 }, headers: { Authorization: "Bearer #{create_mock_token}" }
+    assert_response 200
+  end
+
+  test "create action should return 422" do
+    create_mock_user
+
+    post "/comments", params: { user_id: 1, post_id: 1 }, headers: { Authorization: "Bearer #{create_mock_token}" }
+    assert_response 422
+
+    post "/comments", params: { text: "test", post_id: 1 }, headers: { Authorization: "Bearer #{create_mock_token}" }
+    assert_response 422
+
+    post "/comments", params: { text: "test", user_id: 1 }, headers: { Authorization: "Bearer #{create_mock_token}" }
+    assert_response 422
+  end
+
+  test "create action should return 401" do
+    post "/comments", params: { text: "test", user_id: 1, post_id: 1 }
+    assert_response 401
+  end
 end
