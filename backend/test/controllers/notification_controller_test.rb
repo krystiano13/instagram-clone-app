2063182e959fb16005090a6d19e1977eaa5bbf09
krystiano13 <crystianotv@gmail.com>
2024-11-08 21:19:33 +0100

    create tests for notifications controller

--- backend/test/controllers/notification_controller_test.rb
+++ backend/test/controllers/notification_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class NotificationControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return array with 200 code" do
+    create_mock_user
+    get "/notifications/1", headers: { Authorization: "Bearer #{create_mock_token}" }
+    assert_response 200
+  end
+
+  test "should return count with 200 code" do
+    create_mock_user
+    get "/notifications/count/1", headers: { Authorization: "Bearer #{create_mock_token}" }
+    assert_response 200
+  end
+
+  test "should return 401 error" do
+    get "/notifications/count/1"
+    assert_response 401
+
+    get "/notifications/1"
+    assert_response 401
+  end
 end
