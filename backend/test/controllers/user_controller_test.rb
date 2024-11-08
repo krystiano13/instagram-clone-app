613474859b4f7783b2ca8e0115f0021bd140db9f
krystiano13 <crystianotv@gmail.com>
2024-11-08 21:28:36 +0100

    write tests for user controller

--- backend/test/controllers/user_controller_test.rb
+++ backend/test/controllers/user_controller_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 class UserControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create user" do
+    post "/user", params: { user_name: "testuser", email: "test@user", password: "123456" }
+    assert_response 201
+  end
+
+  test "should not create user" do
+    post "/user", params: { email: "test@user", password: "123456" }
+    assert_response 503
+
+    post "/user", params: { user_name: "testuser", password: "123456" }
+    assert_response 503
+
+    post "/user", params: { user_name: "testuser", email: "test@user" }
+    assert_response 503
+  end
+
+  test "should update user" do
+    create_mock_user
+    patch "/user/1", headers: { Authorization: "Bearer #{create_mock_token}" }, params: { description: "test" }
+    assert_response 200
+  end
+
+  test "should not update user" do
+    create_mock_user
+    patch "/user/1", params: { description: "test" }
+    assert_response 401
+  end
+
+  test "should search user" do
+    create_mock_user
+    get "/user/search/test", headers: { Authorization: "Bearer #{create_mock_token}" }
+    assert_response 200
+  end
+
+  test "should not search user" do
+    create_mock_user
+    get "/user/search/test"
+    assert_response 401
+  end
+
+  test "should show single user" do
+    create_mock_user
+    get "/user/1", headers: { Authorization: "Bearer #{create_mock_token}" }
+    assert_response 200
+  end
+
+  test "should not show single user" do
+    create_mock_user
+    get "/user/1"
+    assert_response 401
+  end
 end
